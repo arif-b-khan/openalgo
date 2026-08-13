771be31d05df805e917e1eda29faa71d26dcf08c
Arif Khan <arif.bannehasan@gmail.com>
2026-08-13 18:36:09 +0530

    add GlobalUsings.cs for csc direct builds
    When building with the Roslyn csc compiler directly (bypassing MSBuild,
    which applies ImplicitUsings from the csproj), the source files lack
    implicit 'using System;' etc. This global-usings file provides them so
    the SDK can be compiled with plain csc.

---
+++ src/OpenAlgo.NET/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Implicit usings normally provided by <ImplicitUsings>enable</ImplicitUsings>
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
